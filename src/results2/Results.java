004939d0f0b4411cb9ee3abb8347de92ecd8d928
TomWhite46 <tomrwhite@hotmail.co.uk>
2021-06-30 16:25:32 +0100

    tidy

---
+++ src/results2/Results.java
@@ -0,0 +1,39 @@
+package results2;
+
+public class Results {
+
+	public int physics;
+	public int chemistry;
+	public int biology;
+	public int total;
+	public double percentage;
+	public String name;
+
+	public Results(String first_name, int physMark, int chemMark, int bioMark) {
+		this.name = first_name;
+		this.physics = physMark;
+		this.chemistry = chemMark;
+		this.biology = bioMark;
+		this.total = physics + chemistry + biology;
+		this.percentage = (total * 100.0) / 450.0;
+	}
+
+	public void showAll() {
+		showResults();
+		showPercentage();
+	}
+
+	public void showResults() {
+		System.out.println(name + "'s results are as follows:");
+		System.out.println("Physics: " + physics + "/150");
+		System.out.println("Chemistry: " + chemistry + "/150");
+		System.out.println("Biology: " + biology + "/150");
+		System.out.println("Total: " + total + "/450");
+	}
+
+	public void showPercentage() {
+		System.out.println(name + "'s overall percentage: " + percentage + "%");
+		System.out.println();
+	}
+
+}
