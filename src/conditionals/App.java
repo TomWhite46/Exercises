0da9d73b943abe2ee2d96fd97c32854eba64034c
TomWhite46 <tomrwhite@hotmail.co.uk>
2021-07-01 09:31:38 +0100

    shorter Conditional

--- src/conditionals/App.java
+++ src/conditionals/App.java
@@ -4,6 +4,7 @@ public class App {
 
 	public static void main(String[] args) {
 		Conditionals.fizzBuzz(15);
+		ConditionalsAlt.fizzBuzz(15);
 	}
 
 }
---
+++ src/conditionals/ConditionalsAlt.java
@@ -0,0 +1,17 @@
+package conditionals;
+
+public class ConditionalsAlt {
+
+	public static void fizzBuzz(int i) {
+
+		if (i % 3 == 0) {
+			System.out.print("Fizz");
+		}
+
+		if (i % 5 == 0) {
+			System.out.print("Buzz");
+		}
+
+	}
+
+}
