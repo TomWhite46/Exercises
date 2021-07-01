1d641433b49227e32eb151a8806c946460f91fe6
TomWhite46 <tomrwhite@hotmail.co.uk>
2021-07-01 10:01:00 +0100

    new switch

---
+++ src/switches/App.java
@@ -0,0 +1,11 @@
+package switches;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		Switch.weekDays(8);
+
+	}
+
+}
---
+++ src/switches/Switch.java
@@ -0,0 +1,32 @@
+package switches;
+
+public class Switch {
+
+	public static void weekDays(int dayNum) {
+		switch (dayNum) {
+		case 1:
+			System.out.println("Monday");
+			break;
+		case 2:
+			System.out.println("Tuesday");
+			break;
+		case 3:
+			System.out.println("Wednesday");
+			break;
+		case 4:
+			System.out.println("Thursday");
+			break;
+		case 5:
+			System.out.println("Friday");
+			break;
+		case 6:
+			System.out.println("Saturday");
+			break;
+		case 7:
+			System.out.println("Sunday");
+			break;
+		default:
+			System.out.println("Only 7 days!");
+		}
+	}
+}
